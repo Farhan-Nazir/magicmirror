cb5ac9e2d00f79fce13c04dd3f403fcf81136c91
Farhn-Nazir <nazirf3440@gmail.com>
2018-12-25 00:14:30 +0100

    new change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: true }));
 
 // Listen App
-let port = process.env.PORT || 3000; //Use on server.
+let port = process.env.PORT || 8080; //Use on server.
 let server = app.listen(port, () => {
   console.log("App started on port", port);
 });
